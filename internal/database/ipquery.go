12305ace5d6764db63f5f4de9214c5710397c2d4
Mason <mason.francis@beehive.net>
2024-06-25 12:05:11 -0600

    Added UUID for requests so they are better differentiated
    Tracking them by IP alone would not track all requests. If multiple requests from the same
    IP came in at the same time, they would only be recorded in the DB as one request.
    
    That is now fixed

--- internal/database/ipquery.go
+++ internal/database/ipquery.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"github.com/google/uuid"
 	"github.com/redis/go-redis/v9"
 	"strconv"
 	"time"
@@ -11,30 +12,25 @@ import (
 // Returns true if the IP has made more requests than the rate limit, false otherwise
 // Returns an error if there was an error checking the IP
 func CheckIp(rdb *redis.Client, ip string, dbCtx context.Context, timeWindow int, rateLimit int) (bool, error) {
-
-	// Get the current time
 	now := time.Now().Unix()
-
-	// Get the start time for the time window
 	startTime := now - int64(timeWindow)
 
-	// Record the request in the sorted set with the IP as the key and the timestamp as the score
-	_, err := rdb.ZAdd(dbCtx, ip, redis.Z{Score: float64(now), Member: strconv.FormatInt(now, 10)}).Result()
+	// Generate a unique identifier for the request
+	uid := uuid.New().String()
 
-	// Get the number of requests made by the IP with a timestamp between the start time and now
+	// Use the unique identifier as the member in the sorted set
+	_, err := rdb.ZAdd(dbCtx, ip, redis.Z{Score: float64(now), Member: strconv.FormatInt(now, 10) + ":" + uid}).Result()
+
 	count, err := rdb.ZCount(dbCtx, ip, strconv.FormatInt(startTime, 10), strconv.FormatInt(now, 10)).Result()
 	if err != nil {
 		return false, err
 	}
 
-	// Remove requests that are outside the time window + 2 seconds
-	_, err = rdb.ZRemRangeByScore(dbCtx, ip, "-inf", strconv.FormatInt(startTime-2, 10)).Result()
+	_, err = rdb.ZRemRangeByScore(dbCtx, ip, "-inf", strconv.FormatInt(startTime-1, 10)).Result()
 
-	// If the number of requests is greater than the rate limit, return true
 	if count > int64(rateLimit) {
 		return true, nil
 	}
 
-	// If the number of requests is less than or equal to the rate limit, return false
 	return false, nil
 }
